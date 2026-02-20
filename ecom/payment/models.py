aab442fbe7b5ba94cc96d228703c5185b7be600a
keertiam8 <keertiam8@gmail.com>
2026-02-20 14:43:28 +0530

    custom order models

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
+
 # Create your models here.
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -17,3 +19,23 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f"Shipping Address - {str(self.id)}"
+    
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=500, blank=True)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order - {str(self.id)}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"OrderItem - {str(self.id)}"
